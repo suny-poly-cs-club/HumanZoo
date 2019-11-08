9a993cd7104fdbcee5be7eaa6da8549c0a2dbffd
Daura951 <murphyd6@sunypoly.edu>
2019-11-07 20:39:24 -0500

    Added image from file logic

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,19 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class Main : MonoBehaviour
 {
+    int index = 0;
+    string path = "Assets/Resources/scannedImages/";
     public GameObject characterRectangle;
+    FileInfo[] files;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(characterRectangle, new Vector2(0, 0), Quaternion.identity);
+       
+        DirectoryInfo directory = new DirectoryInfo(path);
+        files = directory.GetFiles("*.*");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        import();
+    }
+
+    public void import()
+    {
+        var scannedSprite = getImage();
+        print(scannedSprite);
+        if(scannedSprite!=null)
+        {
+            GameObject character = Instantiate(characterRectangle, new Vector2(0, 0), Quaternion.identity);
+            character.GetComponent<SpriteRenderer>().sprite = scannedSprite;
+        }
+    }
+
+     Sprite getImage()
+    {
+        DirectoryInfo directory = new DirectoryInfo(path);
+        FileInfo[] newFiles = directory.GetFiles("*.*");
+
+        List<FileInfo> addedFiles = new List<FileInfo>();
+        foreach(FileInfo f in newFiles)
+        {
+            bool isSame = false;
+            foreach(FileInfo g in files)
+            {
+                if(g==f)
+                {
+                    isSame = true;
+                }
+            }
+            if(isSame)
+            {
+                addedFiles.Add(f);
+            }
+        }
+        string s = "[";
+        foreach(FileInfo name in addedFiles)
+        {
+            s += name + ",";
+        }
+        s += "]";
+        print(s);
+
+        print(index);
+        if(addedFiles.Count>0)
+        {
+            index++;
+            FileInfo file = addedFiles[0];
+            string fileName = file.Name;
+            print(fileName);
+            var scannedSprite = Resources.Load<Sprite>("scannedImages/"+fileName);
+            return scannedSprite;
+        }
+        else
+        {
+            return null;
+        }
+      
     }
 }
